45755e0b74a4946c831dd7f17f9edeaa696ee1bb
AbdumalikBebeza <sultan.bebeza@gmail.com>
2023-08-15 17:49:12 +0600

    add set curs

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 from aiogram import Bot, Dispatcher
 
 
+ADMINS = [908379438, 5517017632]
 COINGECKO_API_URL = 'https://api.coingecko.com/api/v3/simple/price?ids=litecoin&vs_currencies=usd'
 TOKEN = "6401593788:AAFTNOJfR1hIUTaIgN91TGzX12ce4uwnVQg"
 bot = Bot(token=TOKEN)
